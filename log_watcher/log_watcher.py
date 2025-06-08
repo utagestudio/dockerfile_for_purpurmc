c227b047b5038539e66c68ce74ce9c00e06bb234
Kotaro Kawashima <utageyanagi+kkgithub@gmail.com>
2025-06-08 20:17:46 +0900

    Refactor log watcher implementation and optimize dependencies.
    Reorganized the `log_watcher` code by replacing `requests` with `pynotificator` for cleaner Discord notification handling. Added a proper `requirements.txt` and updated the `Dockerfile` to streamline dependency management. Minor adjustments were made to the `docker-compose.yml` for better flexibility.

--- log_watcher/log_watcher.py
+++ log_watcher/log_watcher.py
@@ -4,10 +4,9 @@ import os
 import re
 import time
 
-import requests
-from watchdog.events import FileSystemEventHandler
 from watchdog.observers.polling import PollingObserver
-
+from watchdog.events import FileSystemEventHandler
+from pynotificator import DiscordNotification
 
 WEBHOOK_URL = os.environ['WEBHOOK_URL']
 TARGET_DIR = "/opt/minecraft/logs/" #監視対象のフォルダ
@@ -19,12 +18,14 @@ MESSAGE_PATTERNS = [
     # イベントを追加したい場合はここに追加する
 ]
 
+# Discordに送るメッセージを生成する
 def message_creation(text: str):
     for pattern, message_format in MESSAGE_PATTERNS:
         match = re.search(pattern, text)
         if match:
             return message_format.format(*match.groups())
     return None
+
 
 class MinecraftLogMonitor:
     def __init__(self, target_dir, target_file, webhook_url):
@@ -34,9 +35,8 @@ class MinecraftLogMonitor:
         self.log_position = 0
 
     def send_message(self, message: str) -> None:
-        main_content = {"content": message}
-        response = requests.post(self.webhook_url, json=main_content)
-        response.raise_for_status()
+        dn = DiscordNotification(message, self.webhook_url)
+        dn.notify()
 
     def get_log(self, filepath: str):
         with open(filepath, "r", errors="ignore") as f:
@@ -48,7 +48,6 @@ class MinecraftLogMonitor:
             text = message_creation(log)
             if text:
                 self.send_message(text)
-
 
 class ChangeHandler(FileSystemEventHandler):
     def __init__(self, monitor: MinecraftLogMonitor):
@@ -63,15 +62,15 @@ class ChangeHandler(FileSystemEventHandler):
             self.monitor.get_log(filepath)
 
 
+
+
 if __name__ == "__main__":
     monitor = MinecraftLogMonitor(TARGET_DIR, TARGET_FILE, WEBHOOK_URL)
     event_handler = ChangeHandler(monitor)
+
     observer = PollingObserver()
-    observer.schedule(event_handler, monitor.target_dir, recursive=True)
+    observer.schedule(event_handler, monitor.target_dir, recursive=False)
     observer.start()
-    try:
-        while True:
-            time.sleep(0.1)
-    except KeyboardInterrupt:
-        observer.stop()
-    observer.join()
+
+    while True:
+        time.sleep(1)
